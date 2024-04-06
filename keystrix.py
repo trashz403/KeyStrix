07ac67909b62d8d3ab3866e0331b5cfd828ba1ff
TRASHZ403 <107699834+trashz403@users.noreply.github.com>
2024-04-06 00:34:17 -0500

    display Keystrix.py

--- keystrix.py
+++ keystrix.py
@@ -3,3 +3,30 @@ from colorama import Fore, Back, Style
 import pynput
 from pynput.keyboard import Key, Listener
 import datetime
+
+
+colorama.init()
+
+text = """
+8  dP 8888 Yb  dP .d88b. 88888 888b. 888 Yb  dP 
+8wdP  8www  YbdP  YPwww.   8   8  .8  8   YbdP  
+88Yb  8      YP       d8   8   8wwK'  8   dPYb  
+8  Yb 8888   88   `Y88P'   8   8  Yb 888 dP  Yb 
+"""
+special_text = "KEYSTRIX"
+username = "<@trashz403>"
+
+# colors for characters
+colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
+
+# Printing characters with color
+color_index = 0
+for char in text:
+    if char != '\n':
+        print(colors[color_index] + char, end="")
+        color_index = (color_index + 1) % len(colors)
+    else:
+        print()  # Move to the next line
+
+print("\n","            ",username)
+print("\n\nhttps://github.com/trashz403")
